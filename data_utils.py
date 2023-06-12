fab7a909f5aad40870e44030f4387fc5aaea7cf5
Pratiksha Garode <pratikshagarode100@gmail.com>
2023-06-12 08:52:01 +0200

    adding comments

--- data_utils.py
+++ data_utils.py
@@ -7,3 +7,5 @@ current_dir = os.getcwd() + ""
 all_unequal_instances_objects = pickle.load(open(current_dir + "\\Results\\all_unequal_instances_objects_Hannover.p", "rb"))
 all_complete_solver = pickle.load(open(current_dir + "\\Results\\all_complete_solver.p", "rb"))
 print('data has been loaded successfully ')
+
+# exec(open("data_utils.py").read())
