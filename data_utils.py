5d0843bd9bda5a638d982c4e1fe09a6e8fbf4975
Pratiksha Garode <pratikshagarode100@gmail.com>
2023-06-12 11:15:50 +0200

    adding a function

--- data_utils.py
+++ data_utils.py
@@ -4,8 +4,14 @@ import os
 current_dir = os.getcwd() + ""
 
 
-all_unequal_instances_objects = pickle.load(open(current_dir + "\\Results\\all_unequal_instances_objects_Hannover.p", "rb"))
-all_complete_solver = pickle.load(open(current_dir + "\\Results\\all_complete_solver.p", "rb"))
+def get_data(instance_object= "", solver=""):
+    if instance_object != "":
+        all_unequal_instances_objects = pickle.load(open(current_dir + "\\Results\\all_unequal_instances_objects_Hannover.p", "rb"))
+        return all_unequal_instances_objects
+    if solver != "":
+        all_complete_solver = pickle.load(open(current_dir + "\\Results\\all_complete_solver.p", "rb"))
+        return all_complete_solver
+
 print('data has been loaded successfully ')
 
 # exec(open("data_utils.py").read())
